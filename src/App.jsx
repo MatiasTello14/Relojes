f9f51a21db067ef79919beb91e5f07c50ec30642
Matias Tello <matiastello1@hotmail.com>
2023-03-27 01:04:09 -0300

    primer entrega

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import NavBar from "./componentes/Nabvar/NavBar";
+import ItemListContainer from "./componentes/ItemListContainer/ItemListContainer";
+
+function App() {
+
+  return (
+    <>
+    <NavBar />
+    <ItemListContainer greeting="bienvenidos a nuestro sitio web"/>
+    </>
+  );
+}
+
+export default App;
